0b404574d557321809c717c83f31a672bb2a5002
Stefan Tampu <stefan_tampu@yahoo.com>
2023-12-20 16:16:03 +0200

    create phoneNumberAppear() to make company phone number appear in nav when call button is clicked

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ function closeMenu() {
     hamburger.classList.remove("active");
     nav.classList.remove("active");
 }
+
+const navCall = document.getElementById("nav-call");
+const navCallNumber = document.getElementById("nav-call-number");
+
+(function phoneNumberAppear() {
+    let i = 0;
+    navCall.addEventListener("click", () => {
+        i++;
+        if(i%2 === 1){
+            navCallNumber.style.color = "var(--dark-blue)";
+            navCallNumber.style.transform = "translateY(0px)";
+        } else {
+            navCallNumber.style.color = "var(--light-blue)";
+            navCallNumber.style.transform = "translateY(-12px)";
+        }
+    });
+})();
