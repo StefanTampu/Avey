531797e9957fcf0f5dbcab03d633ac007bed7953
Stefan Tampu <stefan_tampu@yahoo.com>
2023-12-21 08:18:17 +0200

    Make slider functional in index, but so far with only one container Forget last commit

--- script.js
+++ script.js
@@ -41,11 +41,71 @@ const width = window.innerWidth;
     });
 })();
 
+const baImage = document.querySelector(".ba-image")
+document.querySelector(".slider").addEventListener("input", (e) => {
+    baImage.style.setProperty("--position", `${e.target.value}%`)
+})
+
+/*
+const baImage = document.querySelector(".ba-image");
 const baBefore = document.querySelector(".ba-before");
 const baLine = document.querySelector(".ba-line");
 const baLineStyles = window.getComputedStyle(baLine);
+let startPosX = 0;
+let newPosX = 0;
 
-(function drag() {
+(function dragBaLine(){
+    const {
+        y: baImageY
+    } = baImage.getBoundingClientRect();
+
+    baLine.addEventListener("mousedown", function(e) {
+        e.preventDefault();
+        baLine.addEventListener("mouseup", mouseUp);
+        baLine.addEventListener("mousemove", mouseMove);
+    });
+
+    function drag() {
+        let left = baLineStyles.getPropertyValue("left");
+        baBefore.style.width = left;
+    };
+
+    function mouseMove(e) {
+        e.preventDefault();
+        baLine.style.left = (e.clientX - baImageY) + "px";
+        drag();
+    }
+
+    function mouseUp() {
+        baLine.removeEventListener("mouseup", mouseUp);
+        baLine.removeEventListener("mousemove", mouseMove);
+    }
+})();
+
+baLine.addEventListener("mousedown", function(e){
+    startPosX = e.clientX;
+
+    let offsetX = e.clientX - parseInt(window.getComputedStyle(this).left);
+    
+    document.addEventListener("mousemove", function mouseMove(e) {
+        newPosX = startPosX - e.clientX;
+        startPosX = e.clientX;
+        if (baLine.offsetLeft >= 0 && baLine.offsetLeft <= "100%") {
+            baLine.style.left = (baLine.offsetLeft - newPosX) + "px";
+        } else if (baLine.offsetLeft < 0) {
+            baLine.offsetLeft = 0;
+            baLine.style.left = 0 + "px"; 
+        }
+        drag();
+        console.log(newPosX);
+    });
+
+    document.addEventListener("mouseup", function(){
+        document.removeEventListener("mousemove", mouseMove);
+    })
+})
+
+function drag() {
     let left = baLineStyles.getPropertyValue("left");
     baBefore.style.width = left;
-})();
+};*/
